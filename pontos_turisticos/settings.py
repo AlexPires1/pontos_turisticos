553da5010cc1bf03bb36801ab7bc508773c79fdc
AlexPires1 <alexpirespereira747@gmail.com>
2019-11-08 16:15:21 -0300

    segundo commit

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'atracoes',
+    'comentarios',
+    'rest_framework',
+    'avaliacoes',
 ]
 
 MIDDLEWARE = [
