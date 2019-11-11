cc4a28fbdac8bb7f8dff09d39394c92549f3caf1
AlexPires1 <alexpirespereira747@gmail.com>
2019-11-11 15:11:29 -0300

    Endpoint de Enderecos

--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
+
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
@@ -8,6 +11,9 @@ class PontoTuristico(models.Model):
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-      return self.nome
+        return self.nome
