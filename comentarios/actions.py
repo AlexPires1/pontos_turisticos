689add42c21da487a965366fecbc56fea9f7852e
AlexPires1 <alexpirespereira747@gmail.com>
2019-11-19 15:51:54 -0300

    corrigindo erros

--- comentarios/actions.py
+++ comentarios/actions.py
@@ -1,10 +1,10 @@
-def reprova_comentarios(modeladmin, request, queryset):
-    queryset.update(aprovado=False)
+#def reprova_comentarios(modeladmin, request, queryset):
+#    queryset.update(aprovado=False)
 
 
-def aprova_comentarios(modeladmin, request, queryset):
-    queryset.update(aprovado=True)
+#def aprova_comentarios(modeladmin, request, queryset):
+#    queryset.update(aprovado=True)
 
 
-reprova_comentarios.short_description = 'Reprovar comentarios'
-aprova_comentarios.short_description = 'Aprovar comentarios'
+#reprova_comentarios.short_description = 'Reprovar comentarios'
+#aprova_comentarios.short_description = 'Aprovar comentarios'
